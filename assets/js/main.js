1c13643be4ec65f6771c802a46156f7b1ae296d7
Huyen <hoanghuyenn2k@gmail.com>
2022-10-20 07:50:15 +0700

    add index, assets

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            320:{
+              items:1
+            },
+            480:{
+              item:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+  });
